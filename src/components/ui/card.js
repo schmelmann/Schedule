0e38e2290ad5807869a911297fb30eddf7d9b84f
schmelmann <legendishaanxop@gmail.com>
2025-04-24 10:18:55 +0530

    Delete src/components/ui directory

--- src/components/ui/card.js
+++
@@ -1,8 +0,0 @@
-export function Card({ children, className = "" }) {
-  return <div className={`rounded-xl border bg-white ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children, className = "" }) {
-  return <div className={`p-4 ${className}`}>{children}</div>;
-}
-
--- src/components/ui/input.js
+++
@@ -1,8 +0,0 @@
-export function Input({ className = "", ...props }) {
-  return (
-    <input
-      {...props}
-      className={`border rounded px-3 py-2 w-full text-sm focus:outline-none focus:ring-2 focus:ring-pink-400 ${className}`}
-    />
-  );
-}
