ac2b48965b8429d0847094d3f88e86c084046705
schmelmann <legendishaanxop@gmail.com>
2025-04-24 10:00:29 +0530

    Update card.js

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -1,1 +1,8 @@
+export function Card({ children, className = "" }) {
+  return <div className={`rounded-xl border bg-white ${className}`}>{children}</div>;
+}
+
+export function CardContent({ children, className = "" }) {
+  return <div className={`p-4 ${className}`}>{children}</div>;
+}
 
